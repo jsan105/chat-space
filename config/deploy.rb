df4af589b13c3076558a91742c84b07f1a87018a
jsan105 <57351081+jsan105@users.noreply.github.com>
2019-11-20 18:30:57 +0900

    deploy.rb変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 
+set :linked_files, %w{ config/secrets.yml }
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/chatspace.pem'] 
