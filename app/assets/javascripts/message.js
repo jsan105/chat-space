f1b9560f98843ebade15abb558d1fc8ba4ca7f98
jsan105 <57351081+jsan105@users.noreply.github.com>
2019-11-21 15:05:53 +0900

    インクリメンタルサーチ追加　message.js修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -24,7 +24,7 @@ $(function(){
 
 
 
-  $(new_message).on(`submit`, function(e){
+  $('#new_message').on(`submit`, function(e){
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr(`action`);
